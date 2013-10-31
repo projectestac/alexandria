0ad6a096538d872d7e3c95ad11199f4f3a1205e6
mespinosaz <marc.espinosa.zamora@upcnet.es>
2013-10-31 13:43:37 +0100

    Added support for SCORM preview

---
+++ html/web/mod/data/lang/ca/data.php
@@ -0,0 +1,19 @@
+<?php
+//XTEC ************ FITXER AFEGIT - Strings adicionals català
+//2011 - @fcasanel
+//2013.10.30 @author - Marc Espinosa Zamora <marc.espinosa.zamora@upcnet.es>
+$string['ispdipdf'] = 'És un PDF de previsualització de materials PDI?';
+$string['isscormfile'] ='Es tracta d\'un fitxer SCORM?';
+$string['abusereports'] = 'Informes de contingut inadequat';
+$string['reportabuse'] ='Informa de contingut inadequat';
+$string['data_never'] = 'mai';
+$string['reportabuse_select'] ='Selecciona l\'opció que descriu millor el cas de contingut inadequat ';
+$string['reportabuse_copyrights'] ='Infringeix els drets d\'autoria';
+$string['reportabuse_nudity'] ='Té errades greus de forma i/o contingut';
+$string['reportabuse_other'] ='Infringeix una altra condició (cal indicar la descripció a continuació)';
+$string['reportabuse_comments'] ='Afegeix comentaris per ajudar-nos a entendre el teu informe de contingut inadequat.';
+$string['reportabuse_submit'] ='Envia l\'informe';
+$string['reportabuse_cancel'] ='Cancel·la';
+$string['reportabuse_mailsubject'] ='Notificació de contingut inadequat a $a->contentname';
+$string['reportabuse_mail'] =' ha notificat el contingut inadequat següent al recurs $a->coursename: $a->abuse';
+$string['reportabuse_intro'] ='Gràcies per ajudar a la comunitat informant sobre contingut que pot estar infringint les <a href=\"$a\" target=\"_blank\">condicions d\'ús</a> d\'aquest espai! <br>En breu rebràs una còpia a la teva bústia de correu amb el missatge que has notificat.';
---
+++ html/web/mod/scorm/lang/ca/scorm.php
@@ -0,0 +1,6 @@
+<?php
+//XTEC ************ FITXER AFEGIT - Strings adicionals català
+//2011 - @fcasanel
+//2013.10.30 @author - Marc Espinosa Zamora <marc.espinosa.zamora@upcnet.es>
+$string['show_index'] = "Mostra l\'índex >>";
+$string['hide_index'] = "Amaga l\'índex <<";
