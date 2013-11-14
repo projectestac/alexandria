78cb18ea9f3701e651597b123145a40b1b57687f
mespinosaz <marc.espinosa.zamora@upcnet.es>
2013-11-14 14:37:13 +0100

    Fixed a minor issue

--- html/web/mod/data/view_reports.php
+++ html/web/mod/data/view_reports.php
@@ -33,28 +33,30 @@ $actual_page = (isset($_GET['page'])) ? $_GET['page'] : 1;
 //Print page links
 echo '<div class="paging">';
 echo $num_records.' '.get_string("reports", "data").' ('.$num_pages.' '.get_string("pages", "data").')<br/>';
-if ($actual_page<10){
-    $first_page = '1';
-    $last_page = ($num_pages>11) ? 11 : $num_pages;
-    $first = true;
-}elseif ($actual_page >= $num_pages-5){
-    $first_page = $num_pages-10;
-    $last_page = $num_pages;
-    $last = true;
-}else{
-    $first_page = $actual_page-5;
-    $last_page = $actual_page+5;
+if ($num_pages > 1) {
+	if ($actual_page<10){
+	    $first_page = '1';
+	    $last_page = ($num_pages>11) ? 11 : $num_pages;
+	    $first = true;
+	}elseif ($actual_page >= $num_pages-5){
+	    $first_page = $num_pages-10;
+	    $last_page = $num_pages;
+	    $last = true;
+	}else{
+	    $first_page = $actual_page-5;
+	    $last_page = $actual_page+5;
+	}
+	if(!$first) echo '<a href="'.$CFG->wwwroot.'/mod/data/view_reports.php?page=1">1</a>&nbsp;...&nbsp;&nbsp;';
+	for ($i=$first_page; $i<=$last_page; $i++){
+	    if ($i == $actual_page){
+        	echo $i.'&nbsp;&nbsp;';
+	    }else{
+	        echo '<a href="'.$CFG->wwwroot.'/mod/data/view_reports.php?page='.$i.'">'.$i.'</a>&nbsp;&nbsp;';
+	    }
+	}
+	if(!$last && ($num_pages>11)) echo '...&nbsp;<a href="'.$CFG->wwwroot.'/mod/data/view_reports.php?page='.($num_pages).'">'.($num_pages).'</a>';
+	echo '</div>';
 }
-if(!$first) echo '<a href="'.$CFG->wwwroot.'/mod/data/view_reports.php?page=1">1</a>&nbsp;...&nbsp;&nbsp;';
-for ($i=$first_page; $i<=$last_page; $i++){
-    if ($i == $actual_page){
-        echo $i.'&nbsp;&nbsp;';
-    }else{
-        echo '<a href="'.$CFG->wwwroot.'/mod/data/view_reports.php?page='.$i.'">'.$i.'</a>&nbsp;&nbsp;';
-    }
-}
-if(!$last && ($num_pages>11)) echo '...&nbsp;<a href="'.$CFG->wwwroot.'/mod/data/view_reports.php?page='.($num_pages).'">'.($num_pages).'</a>';
-echo '</div>';
 
 //Get reports
 $limitfrom = ($actual_page-1)*$rpp;
