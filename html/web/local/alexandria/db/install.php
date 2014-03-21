6be7045d2964e56c5b9a5667b6b3a4fd1d6bf10f
Pau Ferrer Ocaña <pau.ferrer-ocana@upcnet.es>
2014-03-21 12:43:34 +0100

    Trivial bug change

--- html/web/local/alexandria/db/install.php
+++ html/web/local/alexandria/db/install.php
@@ -5,5 +5,5 @@ defined('MOODLE_INTERNAL') || die();
 function xmldb_local_alexandria_install() {
     global $CFG, $DB;
 
-    upgrade_plugin_savepoint(true, 2014022400, 'local', 'alexandria');
+    //upgrade_plugin_savepoint(true, 2014022400, 'local', 'alexandria');
 }
