c27cc09ce1d901f4da21fefb5c03f53936dad299
pau.ferrer-ocana <crazyserver@gmail.com>
2015-01-19 12:56:18 +0100

    Added modules

---
+++ html/web/blocks/download_statistics/lang/ca/block_download_statistics.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Download Statistics block
+ *
+ * @copyright &copy; 2009-2013 Education Department of Catalonia
+ * @author felix.casanellas@gmail.com
+ * @author marc.espinosa.zamora@upcnet.es
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ */
+
+$string['download_statistics'] = 'Estadístiques de descàrrega';
+$string['total_of_downloads'] = 'Descàrregues totals:';
+$string['courses'] = 'Biblioteca de cursos';
+
+$string['pluginname'] = 'Estadístiques de descàrrega';
+
+$string['download_statistics:addinstance'] = 'Afegir instancia del block Estadístiques de descàrrega';
+
+$string['records'] = 'registres';
+$string['pages'] = 'pàgines';
+$string['resourcename'] = 'Nom';
+$string['resourcedatabase'] = 'Base de dades';
+$string['resourcedownloads'] = 'Descàrregues';
+$string['downloadsreport'] = 'Informe de descàrregues';
+$string['viewreport'] = 'Veure informe';
+
+?>
