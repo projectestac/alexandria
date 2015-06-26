cc9c035d8abbc00342abe70743a2c2a7910d4cc1
crazyserver <pau.ferrer-ocana@upcnet.es>
2015-06-26 11:34:06 +0200

    Change upgrde_base to avoid rebase problems

--- upgrade_base.sh
+++ upgrade_base.sh
@@ -10,15 +10,16 @@ git submodule update --recursive --init
 commit=`git rev-parse HEAD`
 echo Commit $commit
 find . -name '\.git*' -exec rm -rf {} \;
+rm CHANGES.txt
 popd
 
 git checkout AGORA_BASE
+git pull
 rm -rf html/web
 cp -rp /tmp/agora_alexandria_base/ html/web
 git add -A .
 git commit -m 'Import from commit '$commit
 git push origin AGORA_BASE
-
 
 git checkout master
 git rebase AGORA_BASE master
