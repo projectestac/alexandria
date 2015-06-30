b53491bb4b35863254a9a60e32c486c3061dffb6
pau.ferrer-ocana <crazyserver@gmail.com>
2015-06-30 11:59:43 +0200

    Format about course block

--- html/web/blocks/about_course/block_about_course.php
+++ html/web/blocks/about_course/block_about_course.php
@@ -78,7 +78,7 @@ class block_about_course extends block_base {
         $content_text .= '<p>'.$license_img.'<span style="font-size:0.9em">'.get_string('license_warning','block_about_course', $license_url).'</span></p>';
 
         // Metainformation (link to database entry)
-        $content_text .= '<a href="'.$CFG->wwwroot.'/mod/data/view.php?d='.$dataid.'&mode=single&rid='.$rid.'" >'.$OUTPUT->pix_icon('i/info','','moodle',array('class'=>'iconlarge', 'style'=>'margin-right:6px;vertical-align: text-bottom;')).get_string('metainfo','block_about_course').'</a>';
+        $content_text .= '<p><a href="'.$CFG->wwwroot.'/mod/data/view.php?d='.$dataid.'&mode=single&rid='.$rid.'" >'.$OUTPUT->pix_icon('i/info','','moodle',array('class'=>'iconlarge', 'style'=>'margin-right:6px;vertical-align: text-bottom;')).get_string('metainfo','block_about_course').'</a></p>';
 
     	$module = $DB->get_field('modules','id',array('name' => 'data'));
     	$cmid = $DB->get_field('course_modules','id',array('instance' => $dataid, 'module' => $module));
@@ -89,14 +89,22 @@ class block_about_course extends block_base {
     			$filesize = block_about_course_formatBytes($file->get_filesize());
                 $counter = alexandria_get_downloads($rid, $filefieldid);
     			$url = $CFG->wwwroot.'/local/alexandria/data/download.php?rid='.$rid.'&fid='.$filefieldid;
-    			$content_text .= '<br><a href="'.$url.'" onclick="increase_counter('.$rid.','.$filefieldid.');">'.$OUTPUT->pix_icon('a/download_all','','moodle',array('class'=>'iconlarge', 'style'=>'margin-right:6px;vertical-align: text-bottom;')).get_string('download_course','block_about_course').'</a>';
+    			$content_text .= '<p><a href="'.$url.'" onclick="increase_counter('.$rid.','.$filefieldid.');">'.$OUTPUT->pix_icon('a/download_all','','moodle',array('class'=>'iconlarge', 'style'=>'margin-right:6px;vertical-align: text-bottom;')).get_string('download_course','block_about_course').'</a></p>';
     			$content_text .= '<p id="download_text" style="font-size: 0.9em; margin-left: 32px;">('.$filesize.' - '.get_string('downloads','local_alexandria',$counter).')</p>'.
                                 '<script type="text/javascript" src="'.$CFG->wwwroot.'/local/alexandria/data/files.js"></script>';
     		}
     	}
 
-        $content_text .= '<strong>'.get_string('share','block_about_course').'</strong>'.'<!-- AddThis Button BEGIN -->
-                    <div class="addthis_toolbox addthis_default_style "><a class="addthis_button_preferred_1"> </a> <a class="addthis_button_preferred_2"> </a> <a class="addthis_button_preferred_3"> </a> <a class="addthis_button_preferred_4"> </a> <a class="addthis_button_compact"> </a> <a class="addthis_counter addthis_bubble_style"> </a></div>
+        $content_text .= '<div><strong>'.get_string('share','block_about_course').'</strong>
+                    <!-- AddThis Button BEGIN -->
+                    <div class="addthis_toolbox addthis_default_style ">
+                        <a class="addthis_button_preferred_1"> </a>
+                        <a class="addthis_button_preferred_2"> </a>
+                        <a class="addthis_button_preferred_3"> </a>
+                        <a class="addthis_button_preferred_4"> </a>
+                        <a class="addthis_button_compact"> </a>
+                        <a class="addthis_counter addthis_bubble_style"> </a>
+                    </div></div>
                     <script src="http://s7.addthis.com/js/300/addthis_widget.js#pubid=xa-5284bae877c8c08c" type="text/javascript"></script>
                     <!-- AddThis Button END -->';
 
@@ -104,7 +112,5 @@ class block_about_course extends block_base {
 
         $this->content->footer = '<a style = "font-size: 0.9em;" href="'.$CFG->wwwroot.'/local/alexandria/data/report_abuse.php?recordid='.$rid.'">'.get_string('reportabuse','local_alexandria').'</a>';
         return $this->content;
-
     }
-
 }
