e02a27fd0f9f6572aab5c682a0b530d78adb296e
Toni Ginard <toni.ginard@gmail.com>
2016-02-24 13:55:42 +0100

    WORKS: Added check for moodledata directory

--- html/web/works.php
+++ html/web/works.php
@@ -2,12 +2,19 @@
 //XTEC ************ FITXER AFEGIT - Mira si funciona l'espai web
 //2010.09.01
 
-	require_once('config.php');
-	$con = mysql_connect($CFG->dbhost, $CFG->dbuser, $CFG->dbpass);
-	if ($con){
-		mysql_close($con);
-		echo 'Aplicacio:OK';
-	}else{
-		echo 'DB ERROR';
-	}
-?>
+require_once('config.php');
+$dirToCheck = $CFG->dataroot . '/';
+
+$con = mysqli_connect($CFG->dbhost, $CFG->dbuser, $CFG->dbpass);
+if ($con) {
+    mysqli_close($con);
+    echo 'Aplicacio:OK';
+} else {
+    echo "No s'ha pogut connectar al servidor de bases de dades $CFG->dbhost<br />";
+}
+
+// Check moodledata
+if (!is_writable($dirToCheck)) {
+    $state = ($state == 'Aplicacio:OK') ? '' : $state;
+    $state .= "El directori de dades del <strong>Moodle</strong> ($dirToCheck), o bé no està muntat o bé no té permís d'escriptura.<br>";
+}
