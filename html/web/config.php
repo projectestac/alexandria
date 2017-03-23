c33e186f002cce040e20d9cc115bfd1f6cbd10e1
Marc Català <mcatala@itteria.cat>
2017-03-23 12:27:12 +0100

    Config file outside code

--- html/web/config.php
+++ html/web/config.php
@@ -1,30 +1,10 @@
 <?php  // Moodle configuration file
+
 unset($CFG);
 global $CFG;
-
 $CFG = new stdClass();
-$CFG->dbtype    = 'mysqli';
-$CFG->dblibrary = 'native';
-$CFG->dbhost    = 'localhost';
-$CFG->dbname    = 'alexandria';
-$CFG->dbuser    = 'root';
-$CFG->dbpass    = 'agora';
-$CFG->prefix    = 'mdl_';
-$CFG->dboptions = array (
-  'dbpersist' => 0,
-  'dbsocket' => '',
-);
-$CFG->wwwroot   = 'http://agora-virtual.xtec.cat/alexandria';
-$CFG->dataroot  = '/dades/alexandria/docs';
-$CFG->admin     = 'admin';
 
-$CFG->directorypermissions = 0777;
-
-$CFG->passwordsaltmain = '/PN~dd>bLwqEOKaMa3j9cn*vcb';
-$CFG->memcache_servers = '127.0.0.1';
-
-//Mail information
-$CFG->apligestenv = 'LOCAL';
+require_once(__DIR__ . '/../../config-moodle.php');
 
 require_once(dirname(__FILE__) . '/settings.php');
 
