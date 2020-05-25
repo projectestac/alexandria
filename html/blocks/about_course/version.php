ad64532cb9cd81dbcedb6d5e53a6f992ec386c73
Toni Ginard <toni.ginard@gmail.com>
2020-05-26 01:41:00 +0200

    Added Alexandria blocks and module

---
+++ html/blocks/about_course/version.php
@@ -0,0 +1,7 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2013111900;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2012112900;        // Requires this Moodle version
+$plugin->component = 'block_about_course'; // Full name of the plugin (used for diagnostics)
---
+++ html/local/alexandria/lang/en/local_alexandria.php
@@ -0,0 +1,83 @@
+<?php
+$string['pluginname'] = 'Alexandria';
+$string['alexandria'] = 'Alexandria';
+
+$string['abusereports'] = 'Reports about inappropiate content';
+$string['reportabuse'] = 'Report about inappropiate content';
+
+$string['data_never'] = 'never';
+$string['reportabuse_select'] = 'Select the option that better describes the inappropiate content ';
+$string['reportabuse_copyrights'] = 'Violates the author\'s rights';
+$string['reportabuse_nudity'] = 'It has grave errors of form or content';
+$string['reportabuse_other'] = 'Violates another condition (You must specify it)';
+$string['reportabuse_comments'] = 'Add comments to help us to understand your report about inappropiate content.';
+$string['reportabuse_submit'] = 'Send the report';
+$string['reportabuse_cancel'] = 'Cancel';
+$string['reportabuse_mailsubject'] = 'Report about inappropiate content in $a->coursename';
+$string['reportabuse_mail'] = ' had reported the following inappropiate content in $a->coursename: $a->abuse';
+$string['reportabuse_intro'] = 'Thank you for help the comunity reporting about contents that may violate the <a href="{$a}" target="_blank">terms of use</a> of this place! <br>Soon you\'ll recieve a copy of your report in your mailbox.';
+$string['reportabuse_noguestuseage'] = 'Guests are not allowed to report.';
+$string['reportabuse_no_comments'] = 'No comments';
+$string['reportabuse_no_topic'] = 'No topic';
+
+$string['licensefield'] = 'License';
+$string['creatorfield'] = 'Author';
+
+$string['coursesdataid'] = 'List of course IDs that contain course-based databases';
+$string['coursefieldid'] = 'Database course ID field';
+$string['fullnamefieldid'] = 'Database fullname field';
+$string['shortnamefieldid'] = 'Database shortname field';
+$string['summaryfieldid'] = 'Database summary field';
+$string['creatorfieldid'] = 'Database creator field';
+$string['categoryfieldid'] = 'Database category field';
+$string['urlfieldid'] = 'Database URL field';
+$string['licensefieldid'] = 'Database licence field';
+$string['creationdatefieldid'] = 'Database creation date field';
+$string['filefieldid'] = 'Database file field';
+$string['moodleversionfieldid'] = 'Database Moodle Version field';
+$string['configmoodleversionfieldid'] = 'Field of the database containing the version of Moodle of the backup file';
+$string['configfilefieldid'] = 'Field of the database containing the file';
+$string['configfullnamefieldid'] = 'Field of the database containing the fullname';
+$string['configshortnamefieldid'] = 'Field of the database containing the shortname';
+$string['configsummaryfieldid'] = 'Field of the database containing the summary';
+$string['configcreatorfieldid'] = 'Field of the database containing the creator';
+$string['configcategoryfieldid'] = 'Field of the database containing the category';
+$string['configurlfieldid'] = 'Field of the database containing the URL';
+$string['configlicensefieldid'] = 'Field of the database containing the licence';
+$string['configcreationdatefieldid'] = 'Field of the database containing the creation date';
+$string['configcoursesdataid'] = 'List of courses that should be handled as a course database.';
+
+$string['fileunavailable'] = 'File not available at this moment';
+$string['recordmissingfield'] = 'Missing necessary field: {$a}';
+$string['error_restoringcourse'] = 'Error restoring course';
+
+$string['reports'] = 'Reports';
+$string['pages'] = 'Pages';
+$string['resource_reported'] = 'Resource reported';
+$string['reporter'] = 'Author of the report';
+$string['report_topic'] = 'Topic';
+$string['report_desc'] = 'Description';
+$string['report_date'] = 'Date';
+
+$string['show_course'] = 'Show course';
+$string['nocourse'] = 'No course';
+
+$string['filetype'] = 'File Type';
+$string['otherfile'] = 'Other';
+$string['pdipdffile'] = 'PDI PDF preview File';
+$string['pdifile'] = 'PDI File';
+$string['scormfile'] = 'SCORM File';
+$string['coursebackupfile'] = 'Course Backup File';
+
+$string['downloads'] = '<span id="download_counter">{$a}</span> downloads';
+$string['last_download'] = 'Last download: ';
+$string['total_downloads'] = 'Total downloads: ';
+$string['preview_resource'] = 'Preview resource';
+$string['preview_hide'] = 'Hide preview';
+$string['file_notavalaible'] = 'File is not avalaible';
+$string['file_notfound'] = 'Fatal error: File not found';
+$string['preview_notavalaible'] = 'Preview not avalaible';
+
+// SCORM
+$string['show_index'] = 'Show index >>';
+$string['hide_index'] = 'Hide index <<';
