d623474d1d0fc03316b4c95f1bb904f7180e3101
Toni Ginard <toni.ginard@gmail.com>
2020-05-26 01:41:00 +0200

    Added Alexandria blocks and module

---
+++ html/blocks/download_statistics/db/access.php
@@ -0,0 +1,15 @@
+<?php
+
+$capabilities = array(
+    'block/download_statistics:addinstance' => array(
+	'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
+
---
+++ html/local/alexandria/data/counter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Increase the counter for specified recorid
+ *
+ * @copyright &copy; 2009 Education Department of Catalonia
+ * @author sara.arjona@gmail.com
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ */
+
+require_once('../../../config.php');
+require_once('datalib.php');
+
+$recordid = required_param( 'recordid', PARAM_INT );
+$fid = required_param( 'fid', PARAM_INT );
+echo alexandria_get_downloads($recordid, $fid) + 1;
