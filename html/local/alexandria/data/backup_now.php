29ef0714ab88b726a2c601734db950bc4b47742d
Toni Ginard <toni.ginard@gmail.com>
2020-06-03 21:29:10 +0200

    Improved the format of most of the files

--- html/local/alexandria/data/backup_now.php
+++ html/local/alexandria/data/backup_now.php
@@ -2,14 +2,16 @@
 /**
  * Increase the counter for specified recorid
  *
- * @copyright &copy; 2009 Education Department of Catalonia
+ * @copyright 2009 Education Department of Catalonia
  * @author sara.arjona@gmail.com
  * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
  */
 
 require_once('../../../config.php');
 require_once('datalib.php');
+
 require_xtecadmin(true);
 
 $courseid = required_param('courseid', PARAM_INT);
+
 alexandria_backup_course($courseid, true);
